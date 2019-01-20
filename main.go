d7e0b2cc67cfa92e868028c721caa786f81495fc
tMatSuZ <tmatsuzaki1@gmail.com>
2019-01-20 17:49:09 +0900

    除外追加、出力をカンマ形式に変更

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	// out, err := exec.Command("jdeps", "--list-deps", "C:\\Users\\tmatsuzaki\\Downloads\\loader.jar").CombinedOutput()
 	// out, err := exec.Command("jar", "-xvf", "C:\\Users\\tmatsuzaki\\Downloads\\loader.jar").CombinedOutput()
 
-	listFiles("C:\\Users\\tmatsuzaki\\Documents\\go\\src\\slim-jre\\BOOT-INF\\lib")
+	listFiles("/home/tmatsuzaki/Downloads/BOOT-INF/lib")
 
 }
 
@@ -24,30 +24,54 @@ func listFiles(searchPath string) {
 		panic(err)
 	}
 
+	removes := []string{"logback-classic", "lombok"}
+
 	modules := make(map[string]struct{})
 	for _, fi := range fis {
-		if !fi.IsDir() {
-			filepath := filepath.Join(searchPath, fi.Name())
-			pos := strings.LastIndex(filepath, ".")
-			if filepath[pos:] == ".jar" {
-				fmt.Println(string(filepath))
-				out, err := exec.Command("jdeps", "--print-module-deps", "-q", filepath).CombinedOutput()
+		if fi.IsDir() {
+			continue
+		}
+		if isRemoveJar(fi.Name(), removes) {
+			continue
+		}
+
+		filepath := filepath.Join(searchPath, fi.Name())
+		pos := strings.LastIndex(filepath, ".")
+		if filepath[pos:] == ".jar" {
+			fmt.Println(string(filepath))
+			out, err := exec.Command("jdeps", "--print-module-deps", "-q", filepath).CombinedOutput()
+			if err != nil {
+				out, err = exec.Command("jdeps", "--print-module-deps", "-q", "--multi-release", "11", filepath).CombinedOutput()
 				if err != nil {
-					out, err = exec.Command("jdeps", "--print-module-deps", "-q", "--multi-release", "11", filepath).CombinedOutput()
-					if err != nil {
-						fmt.Println(err)
-						os.Exit(1)
-					}
-				}
-				outValue := strings.Split(string(out), ",")
-				for _, module := range outValue {
-					newLineRepModule := strings.Replace(module, "\r\n", "\n", -1)
-					newLineRepModule = strings.Replace(newLineRepModule, "\n", "", -1)
-					modules[newLineRepModule] = struct{}{}
-					fmt.Println(modules)
+					fmt.Println(err)
+					os.Exit(1)
 				}
 			}
+			outValue := strings.Split(string(out), ",")
+			if len(outValue) == 1 {
+				continue
+			}
+			for _, module := range outValue {
+				newLineRepModule := strings.Replace(module, "\r\n", "\n", -1)
+				newLineRepModule = strings.Replace(newLineRepModule, "\n", "", -1)
+				modules[newLineRepModule] = struct{}{}
+			}
+		}
+
+	}
+	keys := make([]string, 0, len(modules))
+	for k := range modules {
+		keys = append(keys, k)
+	}
+	fmt.Println(strings.Join(keys, ","))
+
+}
+
+func isRemoveJar(e string, s []string) bool {
+	for _, v := range s {
+		if strings.Contains(e, v) {
+			return true
 		}
 	}
-	fmt.Println(modules)
+	return false
 }
